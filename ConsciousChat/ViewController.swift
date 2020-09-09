a1736175928cb11b2f5b6442df623318823303ce
Daniel Okenka <danokenka@me.com>
2020-09-09 12:37:15 -0400

    Initial Commit

---
+++ ConsciousChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsciousChat
+//
+//  Created by Dan O on 9/9/20.
+//  Copyright © 2020 People Like Us LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
